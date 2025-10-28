a6fa01ca78091ed8bfa225af85c1298239e0eea0
hazem-moo <zoma.9099@gmail.com>
2025-10-28 16:37:44 +0300

    edit config/middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,22 +2,19 @@ module.exports = [
   "strapi::logger",
   "strapi::errors",
   "strapi::security",
-  "strapi::cors",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      origin: [
+        "https://order-restourant-front-gk36uac4t-hazemmoos-projects.vercel.app",
+      ],
+    },
+  },
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
   "strapi::session",
   "strapi::favicon",
   "strapi::public",
-
-  "strapi::errors",
-  {
-    name: "strapi::cors",
-    config: {
-      enabled: true,
-      origin: [
-        "https://order-restourant-front-gk36uac4t-hazemmoos-projects.vercel.app/",
-      ],
-    },
-  },
 ];
